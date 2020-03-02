2e70bf76c2bb68e1b640a19bc22d13e99c70bb40
Gabriel Seals <sealsgj@gmail.com>
2020-03-01 18:08:27 -0600

    added feature to inquire for name and color and yield random animal. also, placed Console.ReadLine to space the action out.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
             if (args.Length == 0)
             {
                 Console.WriteLine("Please enter a dialect: Southern, Northern, Pirate.");
@@ -29,11 +30,19 @@ namespace HelloWorld
                         break;
                 }
             }
+            Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Welcome to the animal section, {name}. What's your favorite color?");
+            string colorChoice = Console.ReadLine();
 
-
-
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(0, 5);
 
             var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan", "Dog", "Cat" };
+            var randomAnimal = animals[randomNumber];
+            Console.WriteLine($"{colorChoice} is an excellent choice. Would you like a {colorChoice} {randomAnimal}?");
+            Console.ReadLine();
 
             foreach (var animal in animals)
             {
